007474fbbf52407c4d68c6d651a12a47945b78b0
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-05-31 01:40:50 +0530

    langchain prompts ui project and about this is about static prompt

---
+++ prompt_ui.py
@@ -0,0 +1,20 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V4-Pro",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+st.header('Research Tool')
+
+user_input = st.text_input("Enter your prompt")
+
+if st.button('Summarize'):
+    result = model.invoke(user_input)
+    st.write(result.content)
