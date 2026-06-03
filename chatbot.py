a0605313d089e2de93ee1cf1444392853c3b5a4d
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-06-03 15:12:04 +0530

    implemented SystemMessage,HumanMessage,AIMessage concepts

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,15 +8,15 @@ llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V4-Pro", task="text-gene
 
 model = ChatHuggingFace(llm=llm)
 
-chat_histroy = []
+chat_histroy = [SystemMessage(content="You are a helpful AI assistant")]
 
 while True:
     user_input = input("You : ")
-    chat_histroy.append(user_input)
+    chat_histroy.append(HumanMessage(content=user_input))
     if user_input == "exit":
         break
     result = model.invoke(chat_histroy)
-    chat_histroy.append(result.content)
+    chat_histroy.append(AIMessage(content=result.content))
     print("AI : ", result.content)
 
 print(chat_histroy)
